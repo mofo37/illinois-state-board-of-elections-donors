4a75ad1042b4522adebfda540c1e70a03a8468b0
Morgan Fogarty <mofo37@gmail.com>
2018-02-04 20:27:55 -0800

    rename app

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module A1Donors
+module IllinoisStateBoardOfElectionsDonors
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
