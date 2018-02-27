7fa02316709a6c581100cef2e76e7e41c0f11b07
Morgan Fogarty <mofo37@gmail.com>
2018-02-26 23:11:43 -0800

    add scope to contribution to search between 5pm and 5pm

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,4 +1,11 @@
 class Contribution < ApplicationRecord
+  scope :on, lambda { |date| where(
+    "contributed_at BETWEEN ? AND ?", 
+    (Time.parse("#{date.year}-#{date.month}-#{date.day} 17:00:00 z") - 1.day),
+    Time.parse("#{date.year}-#{date.month}-#{date.day} 17:00:00 z")
+    ) 
+  }
+
   def payor_and_purpose
     "#{payor} - #{purpose} #{candidate_name}"
   end
