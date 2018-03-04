626a2f4500a97839bdca2d6089ec50c7b46ae2cd
Morgan Fogarty <mofo37@gmail.com>
2018-03-03 18:52:52 -0700

    Add default_scope to Contribution to ensure date in filename is correct

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,10 +1,12 @@
 class Contribution < ApplicationRecord
   scope :on, lambda { |date| where(
-    "contributed_at BETWEEN ? AND ?", 
-    (Time.parse("#{date.year}-#{date.month}-#{date.day} 17:00:00 z") - 1.day),
-    Time.parse("#{date.year}-#{date.month}-#{date.day} 17:00:00 z")
+      "contributed_at BETWEEN ? AND ?", 
+      (Time.parse("#{date.year}-#{date.month}-#{date.day} 17:00:00 z") - 1.day),
+      Time.parse("#{date.year}-#{date.month}-#{date.day} 17:00:00 z")
     ) 
   }
+
+  default_scope { order(contributed_at: :desc) }
 
   def payor_and_purpose
     "#{payor} - #{purpose} #{candidate_name}"
