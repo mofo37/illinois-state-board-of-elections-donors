18a092f84b1ddfb10d77ddc3384f29bfdf490733
Shane Becker <veganstraightedge@gmail.com>
2019-02-18 16:49:45 -0800

    Extract Heroku setup to private method

--- lib/browser.rb
+++ lib/browser.rb
@@ -14,16 +14,8 @@ class Browser
     # add the option for user-data-dir
     options.add_argument user_data_dir
 
-    # let Selenium know where to look for chrome if we have a hint from
-    # heroku. chromedriver-helper & chrome seem to work out of the box on osx,
-    # but not on heroku.
-    if ENV['GOOGLE_CHROME_BIN'].present?
-      chrome_bin = ENV['GOOGLE_CHROME_BIN']
-      options.add_argument 'no-sandbox'
-      options.binary = chrome_bin
-      # give a hint to here too
-      Selenium::WebDriver::Chrome.driver_path = '/app/vendor/bundle/bin/chromedriver'
-    end
+    # TODO: this isn't used or working yet 2019-02-18
+    setup_heroku
 
     # headless!
     # keyboard entry wont work until chromedriver 2.31 is released
@@ -48,5 +40,23 @@ class Browser
 
   def create_directories_if_needed
     FileUtils.mkdir_p chrome_dir
+  end
+
+  def on_heroku?
+    ENV['GOOGLE_CHROME_BIN'].present?
+  end
+
+  def setup_heroku
+    if on_heroku?
+      # TODO: this isn't used or working yet 2019-02-18
+      # let Selenium know where to look for chrome if we have a hint from
+      # heroku. chromedriver-helper & chrome seem to work out of the box on macOS,
+      # but not on heroku.
+      chrome_bin = ENV['GOOGLE_CHROME_BIN']
+      options.add_argument 'no-sandbox'
+      options.binary = chrome_bin
+      # give a hint to here too
+      Selenium::WebDriver::Chrome.driver_path = '/app/vendor/bundle/bin/chromedriver'
+    end
   end
 end
