0b7527f84e2ec02f3b5e82d515bf5f65ec01ddf4
Morgan Fogarty <mofo37@gmail.com>
2020-04-15 22:35:17 -0700

    Style/GuardClause

--- lib/watir_options_factory.rb
+++ lib/watir_options_factory.rb
@@ -44,15 +44,15 @@ class WatirOptionsFactory
   end
 
   def setup_heroku
-    if on_heroku?
-      # TODO: this isn't used or working yet 2019-02-18
-      # let Selenium know where to look for chrome if we have a hint from
-      # heroku. chromedriver-helper & chrome seem to work out of the box on macOS,
-      # but not on heroku.
-      @options.add_argument 'no-sandbox'
-      @options.binary = chrome_bin
-      # give a hint to here too
-      Selenium::WebDriver::Chrome.driver_path = '/app/vendor/bundle/bin/chromedriver'
-    end
+    return unless on_heroku?
+
+    # TODO: this isn't used or working yet 2019-02-18
+    # let Selenium know where to look for chrome if we have a hint from
+    # heroku. chromedriver-helper & chrome seem to work out of the box on macOS,
+    # but not on heroku.
+    @options.add_argument 'no-sandbox'
+    @options.binary = chrome_bin
+    # give a hint to here too
+    Selenium::WebDriver::Chrome.driver_path = '/app/vendor/bundle/bin/chromedriver'
   end
 end
