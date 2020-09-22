369c66d056ebca4531cc74a70f07c006796e030b
Shane Becker <veganstraightedge@gmail.com>
2020-09-22 00:14:48 -0700

    Delete checkbox and click methods in Browser

--- lib/browser.rb
+++ lib/browser.rb
@@ -1,7 +1,7 @@
 class Browser
   extend Forwardable
 
-  def_delegators :@watir, :goto, :html, :close, :link
+  def_delegators :@watir, :goto, :html, :close, :link, :button, :checkbox
 
   def initialize
     @watir = Watir::Browser.new :chrome, options: WatirOptionsFactory.new.options
